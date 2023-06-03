471c1e0bd406a8264f0ced6f7088c6db2df21df0
Paulo <undevoted@proton.me>
2023-06-03 17:10:02 -0500

    refactor: improving code

--- src/class/writer.ts
+++ src/class/writer.ts
@@ -12,41 +12,43 @@ export class Writer {
     Toolkit.run(
       async (tools) => {
         tools.log.debug(`Getting activity for ${opts.gh_username}`)
+
         const events = await tools.github.activity.listPublicEventsForUser({
           username: opts.gh_username,
           per_page: 100
         })
+
         tools.log.debug(`Activity for ${opts.gh_username}, ${events.data.length} events found.`)
-        tools.log.debug(JSON.stringify(opts))
 
         const content: string[] = events.data
           .filter((event: { type: string }) => opts.validated[event.type] && event.type in formatter.EventsSerials)
           .slice(0, opts.max_lines)
           .map((item: Item) => formatter.EventsSerials[item.type](item))
 
-        const readmeContent = readFileSync(`./${opts.target_file}`, 'utf-8').split('\n')
+        const readme = readFileSync(`./${opts.target_file}`, 'utf-8').split('\n')
 
-        let startIdx = readmeContent.findIndex((content) => content.trim() === '<!--START_SECTION:activity-->')
+        let startIdx = readme.findIndex((content) => content.trim() === '<!--START_SECTION:activity-->')
 
         if (startIdx === -1)
           return tools.exit.failure(`Couldn't find the <!--START_SECTION:activity--> comment. Exiting!`)
 
-        const endIdx = readmeContent.findIndex((content) => content.trim() === '<!--END_SECTION:activity-->')
+        const endIdx = readme.findIndex((content) => content.trim() === '<!--END_SECTION:activity-->')
 
-        if (!content.length) tools.exit.success('No events found. Leaving README unchanged with previous activity')
+        if (!content.length) {
+          tools.exit.success('No events found. Leaving README unchanged with previous activity')
+        }
 
-        if (content.length < 5) tools.log.info('Found less than 5 activities')
+        if (content.length < 5) {
+          tools.log.info('Found less than 5 activities')
+        }
 
         if (startIdx !== -1 && endIdx === -1) {
           startIdx++
 
-          readmeContent.splice(startIdx, startIdx + content.length)
+          readme.splice(startIdx, 0, ...content.map((line, idx) => `> - [x] ${idx + 1}. ${line}`))
+          readme.splice(startIdx + content.length, 0, '<!--END_SECTION:activity-->')
 
-          content.forEach((line, idx) => readmeContent.splice(startIdx + idx, 0, `> - [x] ${idx + 1}. ${line}`))
-
-          readmeContent.splice(startIdx + content.length, 0, '<!--END_SECTION:activity-->')
-
-          writeFileSync(`./${opts.target_file}`, readmeContent.join('\n'))
+          writeFileSync(`./${opts.target_file}`, readme.join('\n'))
 
           try {
             await commander.commitCommand(opts)
@@ -54,25 +56,27 @@ export class Writer {
             tools.log.debug('Something went wrong')
             return tools.exit.failure(err)
           }
+
           tools.exit.success('Wrote to README')
         }
 
-        const oldContent = readmeContent.slice(startIdx + 1, endIdx).join('\n')
+        const oldContent = readme.slice(startIdx + 1, endIdx).join('\n')
         const newContent = content.map((line, idx) => `> - [x] ${idx + 1}. ${line}`).join('\n')
 
-        if (oldContent.trim() === newContent.trim()) tools.exit.success('No changes detected')
+        if (oldContent.trim() === newContent.trim()) {
+          tools.exit.success('No changes detected')
+        }
 
         startIdx++
 
-        const readmeActivitySection = readmeContent.slice(startIdx, endIdx)
+        const readmeActivitySection = readme.slice(startIdx, endIdx)
+
         if (!readmeActivitySection.length) {
-          content.some((line, idx) => {
+          content.forEach((line, idx) => {
             if (!line) {
               return true
             }
-            readmeContent.splice(startIdx, startIdx + content.length)
-
-            readmeContent.splice(startIdx + idx, 0, `> - [x] ${idx + 1}. ${line}`)
+            readme.splice(startIdx + idx, 0, `> - [x] ${idx + 1}. ${line}`)
           })
 
           tools.log.success(`Wrote to ${opts.target_file}`)
@@ -80,11 +84,12 @@ export class Writer {
           let count = 0
 
           readmeActivitySection.some((line, idx) => {
-            if (!content[count]) return true
+            if (!content[count]) {
+              return true
+            }
 
             if (line !== '') {
-              readmeContent.splice(startIdx, startIdx + content.length)
-              readmeContent[startIdx + idx] = `> - [x] ${count + 1}. ${content[count]}`
+              readme[startIdx + idx] = `> - [x] ${count + 1}. ${content[count]}`
               count++
             }
           })
@@ -92,7 +97,7 @@ export class Writer {
           tools.log.success(`Updated ${opts.target_file} with the recent activity`)
         }
 
-        writeFileSync(`./${opts.target_file}`, readmeContent.join('\n'))
+        writeFileSync(`./${opts.target_file}`, readme.join('\n'))
 
         try {
           await commander.commitCommand(opts)
